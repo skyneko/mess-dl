f5b23df53505921abdc5974b24cb0878b6c39c49
yui421 <banhgao.nya@gmail.com>
2020-03-14 22:03:10 +0700

    fix download by 0

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /* -- USER CONFIG --  */
 
-const GROUP_ID = "1999497026819916" // id của group muốn tải về, có thể là userID 
+const GROUP_ID = "1069656803112675" // id của group muốn tải về, có thể là userID 
 
-const LIMIT = 100 // số lượng ảnh muốn tải về (tối đa) 
+const LIMIT = 50 // số lượng ảnh muốn tải về (tối đa) 
 
 
 /* -- END USER CONFIG --*/
@@ -18,12 +18,12 @@ const utils = require("./src/utils");
         return console.log("file requestHeader.txt trống, xem hướng dẫn tại: https://github.com/skyneko/mess-dl/blob/master/README.md")
 
     } else {
-        
+
         console.log("fetching data ...")
         let userData = utils.getUserDataFromRequestHeaders(headerString)
         userData.fb_dtsg = await utils.getFb_dtsg(userData.cookie)
-    
-        getData(userData, parseInt(GROUP_ID), LIMIT)
+        
+        if (LIMIT !== 0) getData(userData, parseInt(GROUP_ID), LIMIT)
     }
 
 })()
