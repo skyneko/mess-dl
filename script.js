8e9f3eaba13498fd7b6ea170d1d3fb2b19393e9a
yui421 <banhgao.nya@gmail.com>
2020-03-14 21:04:28 +0700

    add log

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ const utils = require("./src/utils");
         return console.log("file requestHeader.txt trống, xem hướng dẫn tại: https://github.com/skyneko/mess-dl/blob/master/README.md")
 
     } else {
+        
+        console.log("fetching data ...")
         let userData = utils.getUserDataFromRequestHeaders(headerString)
         userData.fb_dtsg = await utils.getFb_dtsg(userData.cookie)
     
